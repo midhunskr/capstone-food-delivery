1c344323429c1b46977295485bfdb577802a4dd6
midhunskr <midhun.shankar.199@gmail.com>
2024-09-18 23:49:55 +0530

    commit 22

--- backend/index.js
+++ backend/index.js
@@ -27,15 +27,6 @@ app.use((req, res, next) => {
 });
 
 
-//Unsafe-inline
-app.use((req, res, next) => {
-  res.setHeader(
-    'Content-Security-Policy', 
-    "style-src 'self' 'unsafe-inline';" // Add 'unsafe-inline'
-  );
-  next();
-});
-
 //Parsing incoming JSON requests and puts the parsed data in req.body 
 app.use(express.json())
 //Parsing incoming cookie
